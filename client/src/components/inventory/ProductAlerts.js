40dce3b8c3ede24ca6271f64bbb4dd339374e007
40059209 <asimpson21@qub.ac.uk>
2020-09-30 22:41:01 +0100

    db

--- client/src/components/inventory/ProductAlerts.js
+++ client/src/components/inventory/ProductAlerts.js
@@ -8,41 +8,43 @@ const ProductAlerts = ({ levels }) => {
       <PredictedOrderModal levels={levels} />
 
       <h6 className="mb-3">Low Product Alerts</h6>
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Avg Weekly Sales</th>
-            <th>Quantity</th>
-          </tr>
-        </thead>
-        <tbody>
-        {/* eslint-disable-next-line */}
-          {levels.map((item) => {
-            if (item.avg_weekly_sales > Number(item.sum)) {
-              return (
-                <tr key={item.product_id}>
-                  <td>{item.product_name}</td>
-                  <td>{Number(item.avg_weekly_sales).toFixed(2)}</td>
-                  {Number(item.sum) === 0 ? (
-                    <td>
-                      <span className="badge badge-pill badge-danger">
-                        Out Of Stock
-                      </span>
-                    </td>
-                  ) : (
-                    <td>
-                      <span className="badge badge-pill badge-warning">
-                        Running Low: {item.sum}
-                      </span>
-                    </td>
-                  )}
-                </tr>
-              );
-            }
-          })}
-        </tbody>
-      </table>
+      <div className="table-responsive-sm">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Avg Weekly Sales</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* eslint-disable-next-line */}
+            {levels.map((item) => {
+              if (item.avg_weekly_sales > Number(item.sum)) {
+                return (
+                  <tr key={item.product_id}>
+                    <td>{item.product_name}</td>
+                    <td>{Number(item.avg_weekly_sales).toFixed(2)}</td>
+                    {Number(item.sum) === 0 ? (
+                      <td>
+                        <span className="badge badge-pill badge-danger">
+                          Out Of Stock
+                        </span>
+                      </td>
+                    ) : (
+                      <td>
+                        <span className="badge badge-pill badge-warning">
+                          Running Low: {item.sum}
+                        </span>
+                      </td>
+                    )}
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
